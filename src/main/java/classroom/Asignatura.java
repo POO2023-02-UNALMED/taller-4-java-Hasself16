d761329fee6f7b69750126c237ccf600d994e8e9
Hasself16 <141966592+Hasself16@users.noreply.github.com>
2023-09-03 11:50:05 -0500

    Solución

--- src/main/java/classroom/Asignatura.java
+++ src/main/java/classroom/Asignatura.java
@@ -10,15 +10,10 @@ public class Asignatura {
 
     public Asignatura() {
         this("Sin nombre",Tipo.FUNDAMENTACION);
-        this(0);
     }
 
     public Asignatura(int codigoInterno,Tipo tipo) {
-        this("Sin nombre", codigoInterno, 0,tipo);
-    }
-
-    public Asignatura(int codigoExterno,Tipo tipo) {
-        this("Sin nombre", 0, codigoExterno,tipo);
+        this("Sin nombre", codigoInterno, codigoInterno,tipo);
     }
 
     public Asignatura(String nombre, Tipo tipo) {
@@ -37,10 +32,6 @@ public class Asignatura {
         this.codigoExterno = codigoExterno;
         this.nombre = nombre;
         this.tipo = tipo;
-    }
-
-    public void cambiarDatos(int codigoInterno) {
-        this.codigoInterno = codigoInterno;
     }
 
     public void cambiarDatos(int codigoExterno) {
